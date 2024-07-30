230a87f59ceeae473d75ec08b162867496f09826
Babouche <clnabergel@hotmail.fr>
2024-07-30 16:55:25 -0400

    Add backend & gitignore

---
+++ backend/src/app_launcher.py
@@ -0,0 +1,29 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def launch():
+    app = setup_app()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+def setup_app() -> FastAPI:
+    app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_credentials=True,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    include_routers(app)
+
+    return app
+
+
+def include_routers(app: FastAPI):
+    # app.include_router(health_router)
+    pass
